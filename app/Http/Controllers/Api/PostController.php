8aaafde0878cdb7b9e6a58a43fe34880603cb4be
Xurxo Agra <xurxo.agra@gmail.com>
2022-01-19 21:40:36 +0100

    Create index function to list posts

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -2,10 +2,19 @@
 
 namespace App\Http\Controllers\Api;
 
+use App\Models\Post;
 use App\Http\Controllers\Controller;
+
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
-    //
+    /**
+     *  Return all posts in json
+     *  @return object 
+     */
+    public function index() 
+    {
+        return Post::all();
+    }
 }
