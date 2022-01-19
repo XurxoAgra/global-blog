9d3c97d7496be5ed44871b82b3f242f9a7d1364d
Xurxo Agra <xurxo.agra@gmail.com>
2022-01-20 00:46:45 +0100

    Restructure the main view

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -7,15 +7,35 @@
     <title>Global Blog</title>
 
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </head>
 <body>
+    <!-- Main Navbar-->
+    <nav class="navbar navbar-dark bg-dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="{{ route('web.posts.index')}}">Home</a>
+        </div>
+    </nav>
+    <!-- Page heading -->
     <div class="blog-header p-3 pb-md-4 mx-auto text-center">
         <h1 class="display-4 fw-normal">@yield('title')</h1>
         <p class="fs-5 text-muted">@yield('subtitle')</p>
       </div>
+    <!-- Main Container -->
     <div class="container">
         @yield('content')
     </div>
+
+    <div class="container">
+        <footer class="py-3 my-4">
+            <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+                <li class="nav-item">
+                    <a class="nav-link px-2 text-muted" href="{{ route('web.posts.index')}}">Home</a>
+                </li>
+            </ul>
+            <p class="text-center text-muted">© 2021 Xurxo Agra</p>
+        </footer>
+    </div>
+
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 </html>
