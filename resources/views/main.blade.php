bbcbee5ce3c6779ed9ec9c4590d9f58e0bf4275d
Xurxo Agra <xurxo.agra@gmail.com>
2022-01-20 00:23:55 +0100

    Create main and index views

---
+++ resources/views/main.blade.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Global Blog</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</head>
+<body>
+    <div class="blog-header p-3 pb-md-4 mx-auto text-center">
+        <h1 class="display-4 fw-normal">@yield('title')</h1>
+        <p class="fs-5 text-muted">@yield('subtitle')</p>
+      </div>
+    <div class="container">
+        @yield('content')
+    </div>
+</body>
+</html>
---
+++ resources/views/post/index.blade.php
@@ -0,0 +1,35 @@
+@extends('main')
+
+@section('title', 'Recent Posts')
+@section('subtitle', 'Show the recent posts')
+
+@section('content')
+<table class="table">
+    <thead>
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Title</th>
+        <th scope="col">Body</th>
+        <th scope="col">Actions</th>
+      </tr>
+    </thead>
+    <tbody>
+        @foreach ($posts as $post)
+            <tr>
+                <th>
+                    <p>  {{ $post->id}}</p>
+                </th>
+                <th>
+                    <p> {{ $post->title }}</p>
+                </th>
+                <th>
+                    <p> {{ substr($post->body, 0, 50) }}...</p>
+                </th>
+                <th>
+                    <a href="{{ route('web.posts.view', $post->id) }}" type="button" class="btn btn-primary">Show</button>
+                </th>
+            </tr>
+        @endforeach
+    </tbody>
+  </table>
+@endsection
