bf285520a67e0567f6a2320e183f8c30656950d6
XurxoAgra <xurxo.agra@gmail.com>
2022-01-19 20:56:15 +0100

    Create post factory to feed the database

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PostFactory extends Factory
+{
+    /**
+     * Get post using the api and feed the database ramdomly
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        $posts = get_posts_api();
+        $rand = rand(0, 100); 
+         
+        return [
+            'title' => $posts[$rand]->title,
+            'body' => $posts[$rand]->body,
+        ];
+    }
+}
