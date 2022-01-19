4471d6ce717086431dfa4cf9bb9cf70d5fbbc69c
Xurxo Agra <xurxo.agra@gmail.com>
2022-01-19 23:36:48 +0100

    Create index and show functions for the posts

---
+++ app/Http/Controllers/Web/PostController.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use App\Models\Post;
+use App\Http\Controllers\Controller;
+
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     *  Return view with all posts
+     *  @return view 
+     */
+    
+    public function index() 
+    {
+        return view('post.index', ['posts' => Post::all()]);
+    }
+    
+    /**
+     * Show the post.
+     *
+     * @param  int  $id
+     * @return Response
+     */
+    public function show(int $id)
+    {
+        return view('post.show', ['post' => Post::findOrFail($id)]);
+    }
+}
