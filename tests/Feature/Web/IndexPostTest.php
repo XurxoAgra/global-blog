a3208fed9657672760dca0fc9a0fb54922509d58
Xurxo Agra <xurxo.agra@gmail.com>
2022-01-20 22:16:22 +0100

    Create web tests

---
+++ tests/Feature/Web/IndexPostTest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace Tests\Feature\Web;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+use App\Models\Post;
+
+class IndexPostTest extends TestCase
+{
+    /**
+     * Check if posts can be listed correctly in the web
+     *
+     * @test
+     * @return void
+     */
+    public function users_can_show_all_prosts()
+    {
+        $posts = Post::factory()->make();
+        
+        $response = $this->get(route('web.posts.index'))
+            ->assertViewIs('post.index')
+            ->assertSeeText('Recent Posts')
+            ->assertSeeText('Show the recent posts')
+            ->assertStatus(200);
+
+        $posts->each(function ($post) use ($response) {
+            $response->assertSee($post->id);
+            $response->assertSee($post->tittle);
+        });
+    }
+}
---
+++ tests/Feature/Web/ShowPostTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature\Web;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+use App\Models\Post;
+
+class ShowPostTest extends TestCase
+{
+    /**
+     * Check if the users can show a post in the web
+     * 
+     * @test
+     * @return void
+     */
+    public function users_can_show_current_post()
+    {
+        $post = Post::factory()->create();
+
+        $response = $this->get(route('web.posts.view', $post->id))
+        ->assertViewIs('post.show')
+        ->assertSeeText($post->title)
+        ->assertSeeText($post->body)
+        ->assertStatus(200);
+    }
+}
